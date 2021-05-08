646ac218909c97c1f1736206ccc2f6c3393cc454
samroodAli <devsamrood@gmail.com>
2021-05-08 20:14:33 +0530

    Fix:Option to save bashrc to .github

--- bash.sh
+++ bash.sh
@@ -5,7 +5,13 @@ function reload(){
 
 #back up bashrc file
 function bashed(){
-  local current_path=$(pwd);
+  # local current_path=$(pwd); optional if u wanna save bashrc to github
   rm ~/bashrc/.bashrc;
   cp ~/.bashrc ~/bashrc/;
+  #optional if u wanna save bashrc to github:
+  # cd ~/bashrc/;
+  # gim f $1 ${@:2};
+  # git push -uf;
+  # unset feat;
+  # cd $current_path;
 }
