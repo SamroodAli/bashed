e6b2388e574db4143cde4912ec8433d2e0d1060d
samroodAli <devsamrood@gmail.com>
2021-05-08 20:18:21 +0530

    Browser:Open localhost in browser with default port set as 3000

--- browser-and-network.sh
+++ browser-and-network.sh
@@ -2,3 +2,13 @@
 function kill-port(){
   kill -9 $(lsof -t -i:$1) 
 }
+
+#open localhost in browser with default port set as 3000
+default_port=3000
+function port(){
+  if [ -z "$1" ]; then
+    sensible-browser http://192.168.100.175:$default_port
+  else
+    sensible-browser http://192.168.100.175:$1
+  fi
+}
