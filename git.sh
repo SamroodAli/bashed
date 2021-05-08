8cc10f4f80514346500935d3ad9f145e4f409b6f
samroodAli <devsamrood@gmail.com>
2021-05-08 20:09:03 +0530

    Git:Add all files and commit without quotes in one command

--- git.sh
+++ git.sh
@@ -3,3 +3,10 @@ function repo() {
     local=$(git config --get remote.origin.url)
     sensible-browser $local || open $local || xdg-open $local
 }
+
+# git add and commit
+function gia(){
+  git add . #comment this line if you dont want to add all files
+  git commit -m "${@}"
+  echo $@
+}
