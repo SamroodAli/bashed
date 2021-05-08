c3fceb8ce9f6d8d20ef12ab949974427b6501863
samroodAli <devsamrood@gmail.com>
2021-05-08 20:10:59 +0530

    Git:Awesome commit message with prepended feature heads in one command

--- git.sh
+++ git.sh
@@ -10,3 +10,36 @@ function gia(){
   git commit -m "${@}"
   echo $@
 }
+
+# Set default for new terminal sessions, deleting any old value
+feat="Feat"
+set current feat
+function feat(){
+  feat=${1^}
+  echo  "Set feature: $feat"
+}
+# View current feature
+alias feat?='echo $feat'
+
+# Commit with beautiful messages with feature headings prepended
+function gim(){
+  local message=${@:2}
+  case $1 in
+    "a")  gia "Asset:${message^}" ;;
+    "x")  gia "Fix:${message^}" ;;
+    "d")  gia "Docs:${message^}" ;;
+    "s")  gia "Style:${message^}" ;;
+    "r")  gia "Refactor:${message^}" ;;
+    "t")  gia "Test:${message^}" ;;
+    "c")  gia "Config:${message^}" ;;
+    "i")  gia "Init:${message^}" ;;
+    "o")  local message=${@:3};gia "${2^}:${message^}";;
+    "M")  gia "Model:${message^}" ;;
+    "V")  gia "View:${message^}" ;;
+    "C")  gia "Controller:${message^}" ;;
+    "f")  feat $2;
+          message=${@:3}
+          gia "$feat:${message^}";;
+      *)  message=$@;gia "${feat}:${message^}";;
+  esac
+} 
