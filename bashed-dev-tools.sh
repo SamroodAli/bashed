9b3f5ed4b78d94dda153093e2422ea299e963444
samroodAli <devsamrood@gmail.com>
2021-05-10 17:25:00 +0530

    Bashed:Bash file remove for bashed-dev-tools file

--- bashed-dev-tools.sh
+++ bashed-dev-tools.sh
@@ -1,4 +1,4 @@
-# Takes all bash scripts and creates a final single bashscript file
+# Takes all bash scripts and creates a final production ready bashscript file
 function build(){
   rm bashed.sh
   for file in *; do
@@ -8,4 +8,23 @@ function build(){
     cat $file >> bashed.sh
    fi
   done
+}
+
+# reload bashrcs file, no need to quit terminal
+function reload(){
+  source ~/.bashrc
+}
+
+
+#back up bashrc file
+function bashed(){
+  # local current_path=$(pwd); optional if u wanna save bashrc to github
+  rm ~/bashrc/.bashrc;
+  cp ~/.bashrc ~/bashrc/;
+  #optional if u wanna save bashrc to github:
+  # cd ~/bashrc/;
+  # gim f $1 ${@:2};
+  # git push -uf;
+  # unset feat;
+  # cd $current_path;
 }
